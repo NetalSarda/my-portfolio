ef4122103f3b0f29be08b562127f665079830e60
NetalSarda <netalsarda11@gmail.com>
2025-02-20 01:30:51 +0530

    Test Change

--- src/components/Home/ContactSection.jsx
+++ src/components/Home/ContactSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContactSection = () => {
   return <div className="lg:h-full h-screen w-full bg-[var(--card-bg)] lg:w-[30%]">
-    Contact Page
+    this is Contact Page
   </div>;
 };
 
