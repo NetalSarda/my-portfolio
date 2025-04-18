8cb0789ac497b3546209a070136253a03086a850
Khanba22 <121488978+Khanba22@users.noreply.github.com>
2025-04-18 14:43:28 +0530

    Updated contact home page

--- src/components/Home/Content/RightContent.jsx
+++ src/components/Home/Content/RightContent.jsx
@@ -1,19 +1,20 @@
 import { MoveUpRight } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const RightContent = () => {
   return (
     <>
-      <div>
+      <div className="flex w-full justify-between items-center">
         <h2 className="text-[var(--card-dark-text-light)]">Get In Touch</h2>
-        <div className="w-full flex justify-end">
+        <Link href={"/contact"}>
           <MoveUpRight
             strokeWidth={1.5}
             width={30}
             height={30}
             className="h-full aspect-square"
           />
-        </div>
+        </Link>
       </div>
       <div>
         <h1 className="text-5xl text-[var(--card-dark-text)]">Contact Me</h1>
